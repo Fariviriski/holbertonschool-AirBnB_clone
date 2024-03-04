9c59c038a28d9c069e48866d355920c993f2c0da
Fariviriski <mafu.is.here@gmail.com>
2024-03-03 23:33:50 -0400

    Added more tests for Amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -12,32 +12,40 @@ class AmenityTests(unittest.TestCase):
     Class AmenityTests provides unit testing for
     the Amenity class.
     """
-    @classmethod
-    def setUp(cls):
-        """
-        Method to set up Amenity classes for use during testing.
-        """
-        cls.a1 = Amenity()
+    def init_test(self):
+        """tests initialization of Amenity"""
+        amenity = Amenity()
+        self.assertIsInstance(amenity, Amenity)
+        self.assertEqual(amenity.name, "")
 
-    @classmethod
-    def tearDown(cls):
-        """
-        Method to tear down Amenity classes for use during testing.
-        """
-        del cls.a1
-        return super().tearDownClass()
+    def test_attributes(self):
+        """testing Amenity's Attr."""
+        amenity = Amenity()
+        amenity.name = "Smoker's Lounge"
+        self.assertEqual(amenity.name, "Smoker's Lounge")
 
-    def test_class_attrs(self):
-        self.assertEqual(self.a1.name, "")
-        self.assertIn('id', self.a1.to_dict())
-        self.assertIn('created_at', self.a1.to_dict())
-        self.assertIn('updated_at', self.a1.to_dict())
-        self.assertIsInstance(self.a1.name, str)
+    def test_toDict(self):
+        """test 2 dict"""
+        amenity = Amenity()
+        amenity.name = "Cocktail Bar"
+        amenity_dict = amenity.to_dict()
+        self.assertEqual(amenity_dict["name"], "Cocktail Bar")
+        self.assertEqual(amenity_dict["__class__"], "Amenity")
 
-    def test_instance_attrs(self):
-        self.a1.name = "wifi"
-        self.assertEqual(self.a1.name, "wifi")
+    def test_fromDict(self):
+        """test from_dic"""
+        amenity_dict = {
+            "name": "Game Room",
+            "__class__": "Amenity",
+            "id": "420",
+            "created_at": "2023-12-17T12:43:30.0",
+            "updated_at": "2024-02-01T09:52:10.1"
+        }
+        amenity = Amenity(**amenity_dict)
+        self.assertEqual(amenity.name, "Game Room")
+        self.assertEqual(amenity.id, "420")
+        self.assertEqual(amenity.created_at.isoformat(), "2023-12-17T12:43:30")
+        self.assertEqual(amenity.updated_at.isoformat(), "2024-02-01T09:52:10.100000")
 
-
-if __name__ == '__name__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
